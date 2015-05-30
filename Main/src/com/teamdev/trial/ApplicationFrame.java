15f9998e5700c25f1d012afac652dada20c2bf15
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2015-05-30 18:36:32 +0300

    Implement UI

--- Main/src/com/teamdev/trial/ApplicationFrame.java
+++ Main/src/com/teamdev/trial/ApplicationFrame.java
@@ -3,6 +3,8 @@ package com.teamdev.trial;
 import javax.swing.*;
 import java.awt.*;
 
+import static java.awt.GridBagConstraints.*;
+
 /**
  * @author Vladimir Ikryanov
  */
@@ -23,7 +25,7 @@ public class ApplicationFrame extends JFrame {
 
     private Component createRightPane() {
         JPanel result = new JPanel(new BorderLayout());
-        result.setMinimumSize(new Dimension(400, 200));
+        result.setMinimumSize(new Dimension(350, 200));
         result.add(createRightCaption(), BorderLayout.NORTH);
         result.add(createRightContent(), BorderLayout.CENTER);
         return result;
@@ -47,9 +49,9 @@ public class ApplicationFrame extends JFrame {
         JPanel contentPane = new JPanel();
         contentPane.setLayout(new GridBagLayout());
         for (int i = 0; i < 50; i++) {
-            contentPane.add(new ReminderPane(), new GridBagConstraints(0, i, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+            contentPane.add(new ReminderPane(), new GridBagConstraints(0, i, 1, 1, 0.0, 0.0, NORTH, HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
         }
-        contentPane.add(Box.createVerticalGlue(), new GridBagConstraints(0, 50, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+        contentPane.add(Box.createVerticalGlue(), new GridBagConstraints(0, 50, 1, 1, 1.0, 1.0, NORTH, BOTH, new Insets(0, 0, 0, 0), 0, 0));
 
         JScrollPane result = new JScrollPane(contentPane);
         result.setBorder(BorderFactory.createEmptyBorder());
@@ -59,12 +61,38 @@ public class ApplicationFrame extends JFrame {
     private Component createLeftPane() {
         JPanel result = new JPanel(new BorderLayout());
         result.add(createLeftCaption(), BorderLayout.NORTH);
+        result.add(createLeftContent(), BorderLayout.CENTER);
         return result;
     }
 
-    private JLabel createLeftCaption() {
-        JLabel result = new JLabel("Customers");
-        result.setFont(result.getFont().deriveFont(20.0f));
+    private Component createLeftContent() {
+        JPanel result = new JPanel(new BorderLayout());
+        result.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        result.add(createPipelinesPane(), BorderLayout.CENTER);
+        return result;
+    }
+
+    private Component createPipelinesPane() {
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new GridBagLayout());
+        for (int i = 0; i < 10; i++) {
+            contentPane.add(new PipelinePane(), new GridBagConstraints(0, i, 1, 1, 0.0, 0.0, NORTH, HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+        }
+        contentPane.add(Box.createVerticalGlue(), new GridBagConstraints(0, 10, 1, 1, 1.0, 1.0, NORTH, BOTH, new Insets(0, 0, 0, 0), 0, 0));
+
+        JScrollPane result = new JScrollPane(contentPane);
+        result.setBorder(BorderFactory.createEmptyBorder());
+        return result;
+    }
+
+    private Component createLeftCaption() {
+        JLabel label = new JLabel("Customers");
+        label.setFont(label.getFont().deriveFont(20.0f));
+
+        JPanel result = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        result.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 0));
+        result.add(label);
+        result.add(new JButton("New Customer"));
         return result;
     }
 }
--- Main/src/com/teamdev/trial/ReminderPane.java
+++ Main/src/com/teamdev/trial/ReminderPane.java
@@ -3,20 +3,24 @@ package com.teamdev.trial;
 import javax.swing.*;
 import java.awt.*;
 
+import static java.awt.GridBagConstraints.*;
+
 /**
  * @author Vladimir Ikryanov
  */
 public class ReminderPane extends JPanel {
     public ReminderPane() {
         setLayout(new GridBagLayout());
-        add(createInfoPane(), new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-        add(new JButton("Send"), new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        add(new JButton("Cancel"), new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        add(createInfoPane(), new GridBagConstraints(
+                0, 0, 1, 1, 1.0, 0.0, WEST, HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+        add(new JButton("Send"), new GridBagConstraints(
+                1, 0, 1, 1, 0.0, 0.0, WEST, NONE, new Insets(0, 0, 0, 0), 0, 0));
+        add(new JButton("Cancel"), new GridBagConstraints(
+                2, 0, 1, 1, 0.0, 0.0, WEST, NONE, new Insets(0, 0, 0, 0), 0, 0));
     }
 
     private Component createInfoPane() {
         JPanel result = new JPanel(new BorderLayout());
-        result.setBackground(Color.red);
         result.add(new JLabel("John Doe"), BorderLayout.CENTER);
         result.add(new JLabel("Send Week email"), BorderLayout.SOUTH);
         return result;
