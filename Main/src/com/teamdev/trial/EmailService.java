90021a4ed2df262f075ca5b3470f00064ebdca77
Vladimir Ikryanov <vladimir.ikryanov@teamdev.com>
2015-06-03 17:19:32 +0300

    Add email template

--- Main/src/com/teamdev/trial/EmailService.java
+++ Main/src/com/teamdev/trial/EmailService.java
@@ -27,9 +27,25 @@ public class EmailService {
         message.setFrom(new InternetAddress(from));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
         message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc));
+        message.setContent(body, "text/html");
         message.setSubject(subject);
-        message.setText(body);
 
         Transport.send(message);
+    }
+
+    public static void main(String[] args) throws MessagingException {
+        send("vladimir.ikryanov@teamdev.com", "vladimir.ikryanov@gmail.com", "", "", "Welcome to JxBrowser Evaluation!", "<p>Hi {firstName},</p>" +
+                "<p>My name is Vladimir. I'm a developer in JxBrowser Team.</p>" +
+                "<p>I'm just checking in to make sure that configuring your project with JxBrowser library went smoothly.</p>" +
+                "<p>If you need any help getting started or in case of any questions related to usage of JxBrowser in your application, just let me know. I will be happy to help.</p>" +
+                "<p>I hope you will enjoy using JxBrowser!</p><p>All the best,</p><table cellpadding='0' border='0'>" +
+                "<tbody><tr><th><a href='https://plus.google.com/u/0/113918825515210809679/posts' style='text-decoration:none;'>" +
+                "<img alt='Vladimir Ikryanov' src='http://www.teamdev.com/img/evaluate-email/vladimir-ikryanov.jpg' width='60'/>" +
+                "</a></th><td width='10'>&nbsp;</td><td style='line-height:1.4;'>" +
+                "<a href='https://plus.google.com/u/0/113918825515210809679/posts' style='text-decoration:none;color:#000;'>" +
+                "<strong>Vladimir Ikryanov</strong><br>" +
+                "<span style='color:#82898B;'>JxBrowser Team</span><br>" +
+                "<span style='color:#82898B;'>TeamDev Ltd.</span>" +
+                "</a></td></tr></tbody></table>");
     }
 }
